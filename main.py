2652fe554a9822677dd55d68f67bae8b875eb812
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-12-24 16:36:26 +0900

    todo.presentation 配下の View を登録する

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 
 from gumo.core.injector import injector
 from todo.bind import bind_todo
-
+from todo.presentation import register_views
 
 injector.binder.install(bind_todo)
 
 app = flask.Flask(__name__)
+blueprint = flask.Blueprint("blueprint", __name__)
+register_views(blueprint=blueprint)
+app.register_blueprint(blueprint=blueprint)
 
 
 @app.route("/")
